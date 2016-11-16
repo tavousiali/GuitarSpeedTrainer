0a9b97f8db40e175a982bc91a3b51bdb59004414
A.Tavoosi <tavousi.ali@gmail.com>
2016-11-16 16:41:22 +0330

    Tavousi

--- app/src/main/java/com/example/atavoosi/guitarspeedtrainer/SimilarAppActivity.java
+++ app/src/main/java/com/example/atavoosi/guitarspeedtrainer/SimilarAppActivity.java
@@ -109,7 +109,7 @@ public class SimilarAppActivity extends BaseNavigationActivity {
     //region METHODS
 
     public boolean isNetworkAvailable(Context context) {
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
