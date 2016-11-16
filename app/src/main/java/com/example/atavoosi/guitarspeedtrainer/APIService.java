de747aba0e6cd27107ea97d981cb9b8fc2e7fb8b
A.Tavoosi <tavousi.ali@gmail.com>
2016-11-16 09:51:13 +0330

    Tavousi

--- app/src/main/java/com/example/atavoosi/guitarspeedtrainer/APIService.java
+++ app/src/main/java/com/example/atavoosi/guitarspeedtrainer/APIService.java
@@ -6,13 +6,14 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface APIService {
-    @GET("dl/d5VoX/lvni21dIqB/similar_app.json")
+    @GET("dl/enLuw/cAO4GGqNI1/similar_app.json")
     Call<SimilarAppModel> getSimilarApp();
 }
 
-class SimilarAppModel{
+class SimilarAppModel {
     public List<SimilarApp> SimilarApp;
-    public class SimilarApp{
+
+    public class SimilarApp {
         public String imageUrl;
         public String title;
         public String desc;
--- app/src/main/java/com/example/atavoosi/guitarspeedtrainer/SimilarAppActivity.java
+++ app/src/main/java/com/example/atavoosi/guitarspeedtrainer/SimilarAppActivity.java
@@ -9,8 +9,12 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 import java.util.concurrent.Callable;
 
 import retrofit2.Call;
@@ -30,6 +34,10 @@ public class SimilarAppActivity extends BaseNavigationActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        super.setContent(R.layout.activity_similar_app);
+        super.onCreate(savedInstanceState);
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://cdn.persiangig.com/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -42,6 +50,31 @@ public class SimilarAppActivity extends BaseNavigationActivity {
             @Override
             public void onResponse(Call<SimilarAppModel> call, Response<SimilarAppModel> response) {
                 Log.d("Ali", response.body().SimilarApp.toString());
+                List<SimilarAppModel.SimilarApp> similarApps = response.body().SimilarApp;
+                SimilarAppModel.SimilarApp firstSimilarApp = similarApps.get(0);
+                SimilarAppModel.SimilarApp secondSimilarApp = similarApps.get(1);
+
+                ImageView imageView = (ImageView) findViewById(R.id.thumbnail);
+                Picasso.with(SimilarAppActivity.this)
+                        .load(firstSimilarApp.imageUrl)
+                        .error(R.drawable.ic_menu) //باید تصحیح شود
+                        .placeholder(R.drawable.image_pre_view)
+                        .into(imageView);
+
+                ((TextView) findViewById(R.id.title)).setText(firstSimilarApp.title);
+                ((TextView) findViewById(R.id.desc)).setText(firstSimilarApp.desc);
+
+
+                ImageView imageView2 = (ImageView) findViewById(R.id.thumbnail2);
+                Picasso.with(SimilarAppActivity.this)
+                        .load(secondSimilarApp.imageUrl)
+                        .error(R.drawable.ic_menu) //باید تصحیح شود
+                        .placeholder(R.drawable.image_pre_view)
+                        .into(imageView2);
+
+                ((TextView) findViewById(R.id.title2)).setText(secondSimilarApp.title);
+                ((TextView) findViewById(R.id.desc2)).setText(secondSimilarApp.desc);
+
             }
 
             @Override
@@ -50,8 +83,6 @@ public class SimilarAppActivity extends BaseNavigationActivity {
             }
         });
 
-        super.setContent(R.layout.activity_similar_app);
-        super.onCreate(savedInstanceState);
     }
 
     @Override
